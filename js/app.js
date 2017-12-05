2f0385437710c25ad99d3756556685321b8de0e0
Fabian Marcus <dev@fabian-marcus.de>
2017-12-05 08:01:33 +0100

    next steps to flipping cards

--- js/app.js
+++ js/app.js
@@ -7,39 +7,47 @@ const insta = fetch(url.replace('{token}', token))
     .then(resp => resp.json())
     .then(json => showInstaImages(json));
 
-
 function showInstaImages(json) {
+    console.log('data', json);
+
     const data = json.data;
     const $container = document.querySelector('.insta');
 
     for (let i=0; i < data.length; i+=1) {
         const date = data[i];
 
-        const card = document.createElement('div');
-        const front = document.createElement('div');
-        const back = document.createElement('div');
+        const 
+            card = document.createElement('div')
+            inner = document.createElement('div'),
+            front = document.createElement('div'),
+            back = document.createElement('div');
+            picture = document.createElement('picture');
+            source = document.createElement('source');
+            img = document.createElement('img');
 
-        const picture = document.createElement('picture');
-        const source = document.createElement('source');
-        const img = document.createElement('img');
-
-        card.className = "col-xs-4 col-sm-4 card";
+        card.className = "col-xs-4 img-responsive card";
+        inner.className = "card__container";
         front.className = "front";
         back.className = "back";
+
+        back.appendChild(document.createTextNode(date.caption.text));
 
         source.srcset = date.images.standard_resolution.url;
         source.media = '(min-width: 769px)';
 
         img.src =  date.images.low_resolution.url;
-        img.className = 'img-responsive';
 
-        picture.appendChild(source);
+        picture.appendChild(source)
         picture.appendChild(img);
-
         front.appendChild(picture);
-        card.appendChild(front);
-        card.appendChild(back);
-
+        inner.appendChild(front)
+        inner.appendChild(back);
+        card.appendChild(inner);
+        
         $container.appendChild(card);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("Hier!");
+});
